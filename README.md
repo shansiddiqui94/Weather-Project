1bb14630c23056369ca08422c951f2eff6a16a39
Jonathanh7 <Jonathanh949@gmail.com>
2020-11-19 04:39:56 -0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Weather-Project
+This is where you will be completing and pull requesting your project
+
+You have to fork and clone this repository
---
+++ main.js
@@ -0,0 +1,3 @@
+const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+//remember, make a .env file and save your key
+const api_key = 'Your key here';
